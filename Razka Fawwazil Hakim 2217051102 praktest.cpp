3f138166a9d65a43097fb66adb856f8c55d84880
RazkaFH <113082658+RazkaFH@users.noreply.github.com>
2022-09-29 13:16:48 +0700

    Add files via upload

---
+++ Razka Fawwazil Hakim 2217051102 praktest.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a;
+	int b;
+	
+	cin>>a;
+	cin>>b;
+	cout<<endl;
+	
+	for (int i = a;i<=b;i+=a){cout<<i<<endl;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	for(int i = 0;i<10000; i++){
+//		cout<<"aku sayang \n";
+//	}
+//}
+
+//	for(int i = 0; i<100; i--){ 
+//	for(int j =0; j<i; j--){
+//		cout<<"*";
+//	}
+//	cout<<endl;
+//	}
+	
+//}
+
+//	int a = 0;
+//	while (a<100){
+//		cout<<" pertemuan ke :" <<a<<endl;a++;
+//	}
+
+
+//	int c = 1;
+//	do{cout<<"perulangan ke :"<<c<<endl;c++;
+	
+	
+//	}while(c<=10);
+}
